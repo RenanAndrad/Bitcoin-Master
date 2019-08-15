e050b45db97f823acd0cee12a5b57a57be2a91fc
RENAN <renan.andrade@cedrotech.com>
2019-08-15 14:42:50 -0300

    Initial commit

---
+++ BitcoinMaster/app/src/androidTest/java/andrade/renan/com/bitcoinmaster/ExampleInstrumentedTest.kt
@@ -0,0 +1,24 @@
+package andrade.renan.com.bitcoinmaster
+
+import androidx.test.InstrumentationRegistry
+import androidx.test.runner.AndroidJUnit4
+
+import org.junit.Test
+import org.junit.runner.RunWith
+
+import org.junit.Assert.*
+
+/**
+ * Instrumented test, which will execute on an Android device.
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+@RunWith(AndroidJUnit4::class)
+class ExampleInstrumentedTest {
+    @Test
+    fun useAppContext() {
+        // Context of the app under test.
+        val appContext = InstrumentationRegistry.getTargetContext()
+        assertEquals("andrade.renan.com.bitcoinmaster", appContext.packageName)
+    }
+}
